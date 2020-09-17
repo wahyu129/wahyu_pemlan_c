54656a1681ddbd3164240c0357a4b5626da38e5a
unknown <wahyutriningsih36gmail.com>
2020-09-17 08:02:44 +0100

    first commit

---
+++ mainbiodata.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int main() 
+{
+	system ("color D");
+	
+	printf("\tBiodata Saya\n\n");
+	
+	printf("Nama	: Wahyu Triningsih\n");
+	printf("NPM	: 19081010024\n");
+	printf("Kelas	: Pemrograman Lanjutan C\n");
+	printf("Alamat	: Surabaya\n");
+	printf("Semester: 3\n");
+
+	return 0;
+}
